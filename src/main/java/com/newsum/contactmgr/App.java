7dfc1e7ed38cdfe364f1e8a068cbe1ebf00478bd
Micah Newsum <mnewsum@glenair.com>
2021-02-09 17:58:44 -0800

    Add method to fetch all contacts, iterate over each and print results.

--- src/main/java/com/newsum/contactmgr/App.java
+++ src/main/java/com/newsum/contactmgr/App.java
@@ -2,12 +2,15 @@ package com.newsum.contactmgr;
 
 import com.newsum.contactmgr.model.Contact;
 import com.newsum.contactmgr.model.Contact.ContactBuilder;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.ServiceRegistry;
+
+import java.util.List;
 
 public class App {
     //Hold a reusable reference to SessionFactory
@@ -23,9 +26,33 @@ public class App {
     public static void main( String[] args )
     {
         Contact contact = new ContactBuilder("Micah","Newsum")
-                                    .withEmail("micahnew90@yahoo.com")
-                                    .withPhone(5622346959L)
-                                    .build();
+                .withEmail("micahnew90@yahoo.com")
+                .withPhone(5622346959L)
+                .build();
+
+        save(contact);
+
+        //Display list of contacts
+        fetchAllContacts().stream().forEach(System.out::println);
+    }
+
+    private static List<Contact> fetchAllContacts()
+    {
+        //Open a session
+        Session session = sessionFactory.openSession();
+
+        //Create criteria
+        Criteria criteria = session.createCriteria(Contact.class);
+
+        //Get a list of Contact objects according to criteria object
+        List<Contact> contacts = criteria.list();
+
+        session.close();
+        return contacts;
+    }
+
+    private static void save(Contact contact)
+    {
         //Open a session
         Session session = sessionFactory.openSession();
 
